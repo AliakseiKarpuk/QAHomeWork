a42068f272a3163d87f2f58b900f84233a683ca9
rodionfisenko <minsk@joingo.com>
2021-07-13 13:35:08 +0300

    add module

--- IsCharsUnique/src/test/java/IsCharsUnique.java
+++
@@ -1,6 +0,0 @@
-public class IsCharsUnique {
-
-    public static void main(String[] args) {
-        System.out.println("Есть ли повторяющиеся символы? " + IsCharsUniqueService.chars("1qwertyui1"));
-    }
-}
